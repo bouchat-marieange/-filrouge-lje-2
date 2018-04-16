01f02a979ea3559e161db66747fc2b5e6d4a290e
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-04-16 10:13:29 +0200

    entrepireses model

---
+++ Les_minis_entreprises_APP/model/entrepises.php
@@ -0,0 +1,31 @@
+<?php 
+
+function get_all()
+{
+    $bdd = new PDO("mysql:host=localhost;dbname=lje_app","root","root",[PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    
+    $stmt = $bdd->prepare("SELECT * FROM entrepises");
+    
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+}
+function investis()
+{
+    $bdd = new PDO("mysql:host=localhost;dbname=lje_app","root","root",[PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $bdd->beginTransaction();
+
+    $stmt = $bdd->prepare("INSERT INTO investissements (id_entrepise,name,email,address,message) VALUES (:id_entrepise,:name,:email,:address,:message)");
+    $stmt->execute($_POST);
+
+    $stmt = $bdd->prepare("UPDATE entrepises SET investissements = investissements+1 WHERE id = :id_entreprise");
+ 
+    $stmt->execute($_POST);
+
+    $bdd->commit();
+
+   // return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
